0c3b410289d7e8f9099fc517d97f81317d341000
Ruslan Semeniuk <semeniukruslan@ukr.net>
2023-04-02 02:48:55 +0200

    update readme

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,6 +1,8 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,11 +21,15 @@ public class Student {
     private Integer id;
 
     @Column(name = "first_name")
+    @NotBlank(message = "firsName should be not empty")
     private String firstName;
 
     @Column(name = "last_name")
+    @NotBlank(message = "lastName should be not empty")
     private String lastName;
 
     @Column(name = "email")
+    @NotBlank(message = "email should be not empty and in valid format")
+    @Email
     private String email;
 }
